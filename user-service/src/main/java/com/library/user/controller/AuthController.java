d79a721c93a8a708e6acb016f49459a8df3451cd
yasser rachih <yassirach17@gmail.com>
2025-05-09 12:40:16 +0100

    change1

--- user-service/src/main/java/com/library/user/controller/AuthController.java
+++ user-service/src/main/java/com/library/user/controller/AuthController.java
@@ -1,7 +1,11 @@
 package com.library.user.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+
+
 
 @Controller
 public class AuthController {
@@ -11,3 +15,4 @@ public class AuthController {
         return "auth"; 
     }
 }
+
--- user-service/src/main/java/com/library/user/controller/UserController.java
+++ user-service/src/main/java/com/library/user/controller/UserController.java
@@ -2,23 +2,21 @@ package com.library.user.controller;
 
 import com.library.user.entity.User;
 import com.library.user.repository.UserRepository;
-import com.library.user.service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
+
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private UserRepository repository;
-    @Autowired
-    private UserService userService;
+    
 
     @GetMapping
     public List<User> getAllUsers() {
@@ -31,19 +29,7 @@ public class UserController {
     }
 
     
-    @PostMapping("/register")
-    public ResponseEntity<User> register(@RequestBody User user) {
-        return ResponseEntity.ok(userService.register(user));
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody Map<String, String> body) {
-        String email = body.get("email");
-        String password = body.get("password");
-        return userService.login(email, password)
-            .map(ResponseEntity::ok)
-            .orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null));
-    }
+   
 
 
 
